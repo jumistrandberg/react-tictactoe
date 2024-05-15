bd35a238c3bf284caecc999bd65a241af88701f7
Jumi <feliciastrandberg@gmail.com>
2024-05-15 19:03:19 +0200

    fixed tile min h w

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -5,12 +5,14 @@ interface TileProps {
   row: number;
   col: number;
 }
-const Tile: React.FC<TileProps> = ({row, col}) => {
+const Tile: React.FC<TileProps> = ({row, col, value}) => {
   const isLeftEdge = col === 0;
   const isTopEdge = row === 0;
   const borderStyling = `${isLeftEdge ? "border-l-2" : ""} ${isTopEdge ? "border-t-2" : ""}`
 
-  return <div className={`tile w-100 h-100 text-3xl border-b-2 border-r-2 ${borderStyling} justify-center items-center flex `}>Tile</div>;
+  return <div className={`tile min-w-[100px] min-h-[100px] text-3xl border-b-2 border-r-2 ${borderStyling} justify-center items-center flex `}>
+    {value}
+  </div>;
 };
 
 export default Tile;
