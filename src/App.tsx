89cd8c2f0cc94047291428e3a0a4d385eea75b81
Jumi <feliciastrandberg@gmail.com>
2024-05-15 15:52:48 +0200

    added content for tailwindconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import Game from './components/Game'
+import Board from "./components/Board";
 
 function App() {
-
   return (
-  <Game />
- 
-  )
+    <>
+      <Board board={[]} handleClick={() => ""} />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import Board from "./Board";
 
 const Game = () => {
-  return (
-    <div>Game</div>
-  )
-}
+  const [board, setBoard] = useState<any>(
+    // make array with 3 rows 3 columns with empty cells
+    Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => null))
+  );
 
-export default Game
+  return (
+    <div className="game">
+      <h1>Tic-Tac-Toe</h1>
+      <Board board={[]} handleClick={() => ""} />
+    </div>
+  );
+};
+
+export default Game;
