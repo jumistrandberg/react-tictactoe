df2624b66096529073f898afd33ab3ef8b6bbc36
Jumi <feliciastrandberg@gmail.com>
2024-05-15 18:50:39 +0200

    fixed border styling

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import Tile from './Tile'
+import React from "react";
+import Tile from "./Tile";
 
 const Board = () => {
   return (
-    <div className='board grid grid-cols-3 grid-rows-3 relative cursor-pointer'>
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-
+    <div className="board grid grid-cols-3 grid-rows-3 relative cursor-pointer">
+      <Tile row={0} col={0} />
+      <Tile row={0} col={1} />
+      <Tile row={0} col={2} />
+      <Tile row={1} col={0} />
+      <Tile row={1} col={1} />
+      <Tile row={1} col={2} />
+      <Tile row={2} col={0} />
+      <Tile row={2} col={1} />
+      <Tile row={2} col={2} />
     </div>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Tile = () => {
-  return <div className="tile w-100 h-100 text-3xl border-b-2 border-r-2 justify-center items-center flex ">Tile</div>;
+interface TileProps {
+  row: number;
+  col: number;
+}
+const Tile: React.FC<TileProps> = ({row, col}) => {
+  const isLeftEdge = col === 0;
+  const isTopEdge = row === 0;
+  const borderStyling = `${isLeftEdge ? "border-l-2" : ""} ${isTopEdge ? "border-t-2" : ""}`
+
+  return <div className={`tile w-100 h-100 text-3xl border-b-2 border-r-2 ${borderStyling} justify-center items-center flex `}>Tile</div>;
 };
 
 export default Tile;
